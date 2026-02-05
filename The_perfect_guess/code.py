4f62db9a99d4b5c65ec9d05d134ca4d3edb7fd1b
Baibhab Karmakar <baibhabkarmakar29@gmail.com>
2026-02-05 10:45:06 +0530

    Changed all the folder names and fixed small issues

--- The_perfect_guess/code.py
+++ The_perfect_guess/code.py
@@ -3,19 +3,21 @@ import random
 computer_value = random.randint(1 , 100)
 no_of_guess = 1
 def takeUserInput():
-    return int(input("Enter the guess from 1 to 1000 : "))
+    return input("Enter the guess from 1 to 1000 or Quit(Q): ")
 
 
 while(True):
     user = takeUserInput()
-    if(user == computer_value):
+    if(user == "Q"):
+        break
+    if(int(user) == computer_value):
         print(f"Wow ! You have guessed it right ! , You have tried for {no_of_guess} times to guess it correct!")
         break
-    elif(user > computer_value):
-        print("Guess it lower!")
+    elif(int(user) > computer_value):
+        print("Guess lower number please!")
 
     else:
-        print("Guess it higher!")
+        print("Guess higher number please!")
 
     no_of_guess += 1
 
